1b5e9f379ac29d7d7a8e2386b48b167d2cf7281c
paul <ophantomo990@gmail.com>
2024-08-11 10:09:02 -0700

    saved changes

--- script.js
+++ script.js
@@ -15,11 +15,32 @@ function change(ndx){
         document.querySelector('#code-js').style.display = 'block';
     }
 }
+const tailwind = `
+<link rel='stylesheet' href='tailwind/tailwind-all.min.css'>
+<link rel='stylesheet' href='tailwind/tailwind-base.min.css'>
+<link rel='stylesheet' href='tailwind/tailwind-components.min.css'>
+<link rel='stylesheet' href='tailwind/tailwind-utilities.min.css'>
+`;
+
 function update_output(){
     const html = htmlEditor.getValue();
-    const css = cssEditor.getValue();
-    const js = jsEditor.getValue();
-    const content = `
+const css = cssEditor.getValue();
+const js = jsEditor.getValue();
+const btn_txt = document.querySelector('#toggle').textContent;
+if(btn_txt == 'ON'){
+    var content = `
+        <html>
+            <head>
+    <style>${css}</style>
+    ${tailwind}
+        </head>
+        <body>
+        ${html}
+        <script>${js}</script>
+        </body>
+    </html>`
+}else{
+    var content = `
         <html>
             <head>
     <style>${css}</style>
@@ -29,5 +50,44 @@ function update_output(){
         <script>${js}</script>
         </body>
     </html>`
+}
 document.querySelector('#frame').srcdoc = content;
 }
+
+let tailwind_f = false;
+let toggle_btn = document.querySelector('#toggle');
+toggle_btn.addEventListener('click', ()=>{
+    const html = htmlEditor.getValue();
+    const css = cssEditor.getValue();
+    const js = jsEditor.getValue();
+    if(!tailwind_f){
+        let content = `
+            <html>
+                <head>
+                ${tailwind}
+        <style>${css}</style>
+            </head>
+            <body>
+            ${html}
+            <script>${js}</script>
+            </body>
+        </html>`
+        toggle_btn.textContent = 'ON';
+        tailwind_f = true;
+        document.querySelector('#frame').srcdoc = content;
+        }else{
+            let content = `
+        <html>
+            <head>
+    <style>${css}</style>
+        </head>
+        <body>
+        ${html}
+        <script>${js}</script>
+        </body>
+    </html>`
+    toggle_btn.textContent = 'OFF';
+            tailwind_f = false;
+            document.querySelector('#frame').srcdoc = content;
+        }
+})
